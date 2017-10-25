2f3e5865223797d73a1e56658cb9166b3f55f5c2
Scoutt Wright <scott@wrightzone.com>
2017-10-25 13:54:58 -0400

    Orders custom files. Adds Box specific file

--- custom/030_env.zsh
+++ custom/030_env.zsh
@@ -1,9 +1,5 @@
 # source "$HOME/.rvm/scripts/rvm"
 
-
-export RAILS_ENV=development
-export RACK_ENV=development
-
 # use Safari to view man files
 # https://apple.stackexchange.com/questions/14524/how-to-view-man-pages-in-a-web-browser
 # https://jonathansblog.co.uk/open-man-pages-in-safari-or-other-browsers-in-osx
